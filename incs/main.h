b7fa873025bbceed6df9f2b5c4203f0f4fbba6af
Baulutor <dbaule@z2r6p4.42lyon.fr>
2023-10-28 21:30:48 +0200

    norme done

--- incs/main.h
+++ incs/main.h
@@ -1,35 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   execution_center.h                                 :+:      :+:    :+:   */
+/*   main.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbaule <dbaule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 13:29:35 by dbaule            #+#    #+#             */
-/*   Updated: 2023/10/28 21:30:02 by dbaule           ###   ########.fr       */
+/*   Created: 2023/10/28 21:10:24 by dbaule            #+#    #+#             */
+/*   Updated: 2023/10/28 21:13:06 by dbaule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXECUTION_CENTER_H
-# define EXECUTION_CENTER_H
+#ifndef MAIN_H
+# define MAIN_H
 
-typedef struct t_exec
+typedef struct t_main
 {
-	char	**exec_cmd;
-	int		*arg_count;
-	int		statut;
-	int		exit_status;
-	int		id;
-	int		ret;
-	int		value_ret;
-	char	**buf;
-	int		i;
-}	t_exec;
-
-void	parent_wait_and_signal(t_exec *ex, pid_t *tab_pid, t_cmd *pip);
-
-int		prep_pipe(t_cmd *pip);
-
-int		checking_pipe(t_list *spt);
+	char	*buff;
+	char	*buf;
+	char	*rd_line;
+	char	*cmd;
+}	t_main;
 
 #endif
--- main.c
+++ main.c
@@ -6,87 +6,114 @@
 /*   By: dbaule <dbaule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 14:04:59 by eslamber          #+#    #+#             */
-/*   Updated: 2023/10/27 20:35:32 by dbaule           ###   ########.fr       */
+/*   Updated: 2023/10/28 21:28:51 by dbaule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "incs/minishell.h"
+#include "incs/main.h"
 
 unsigned char	g_status;
 
+static int	initialise_main(t_main *main, t_cmd *pip, char **env);
+
+static int	launch_minishell(t_cmd *pip, t_main *main);
+
+static int	prepare_prompt(t_cmd *pip, t_main *main);
+
 int	main(int argc, char **argv, char **env)
 {
-	char	*buff;
-	char	*buf;
-	char	*rd_line = NULL;
-	/* int		id; */
-	char	*cmd;
-	(void)	argc;
-	(void)	argv;
-	t_cmd   pip;
-	int		i;
+	t_cmd	pip;
+	t_main	main;
 
-	cmd = NULL;
-	i = 0;
-	pip.here_pipe = NULL;
-	pip.exp_env = NULL;
-	pip.flag = 0;
-	rd_line = NULL;
-	if (getenv("PATH") == NULL)
-		return (0);
-	if (cpy_env(env, &pip) == 1)
+	(void) argv;
+	(void) argc;
+	if (initialise_main(&main, &pip, env) == 1)
 		return (1);
-	if (initialize_exp_env(&pip, pip.env) == 1)
-		return (error(MALLOC, "0"), anihilation(pip.env), anihilation(pip.save_path), 1);
-	using_history();
-	rl_bind_key('\t', rl_complete);
-	pip.hd_history = NULL;
-	pip.ani_flag = 0;
-	g_status = 0;
 	while (1)
 	{
-		pip.status_hd = 0;
-		main_signals();
-		if (pip.hd_history != NULL)
+		if (launch_minishell(&pip, &main) == 1)
+			return (1);
+	}
+	return (0);
+}
+
+static int	initialise_main(t_main *main, t_cmd *pip, char **env)
+{
+	main->cmd = NULL;
+	main->rd_line = NULL;
+	pip->here_pipe = NULL;
+	pip->exp_env = NULL;
+	main->rd_line = NULL;
+	pip->flag = 0;
+	if (getenv("PATH") == NULL)
+		return (0);
+	if (cpy_env(env, pip) == 1)
+		return (1);
+	if (initialize_exp_env(pip, pip->env) == 1)
+		return (error(MALLOC, "0"), anihilation(pip->env), \
+		anihilation(pip->save_path), 1);
+	using_history();
+	rl_bind_key('\t', rl_complete);
+	pip->hd_history = NULL;
+	pip->ani_flag = 0;
+	g_status = 0;
+	return (0);
+}
+
+static int	launch_minishell(t_cmd *pip, t_main *main)
+{
+	if (prepare_prompt(pip, main) == 1)
+		return (1);
+	main->buff = ft_strjoin(main->buf, "$ ");
+	free(main->buf);
+	main->rd_line = readline(main->buff);
+	free(main->buff);
+	if (main->rd_line == NULL)
+	{
+		free(main->rd_line);
+		free_all(pip);
+		return (quit(), 1);
+	}
+	main->rd_line = verif_read(main->rd_line, pip);
+	if (main->rd_line == NULL)
+	{
+		free_all(pip);
+		exit(g_status);
+	}
+	free(main->cmd);
+	if (historic_fct(main->rd_line, pip) == 1)
+		return (free_all(pip), anihilation(pip->hd_history), \
+		free(main->rd_line), 1);
+	free(main->rd_line);
+	return (0);
+}
+
+static int	prepare_prompt(t_cmd *pip, t_main *main)
+{
+	int	i;
+
+	pip->status_hd = 0;
+	main_signals();
+	if (pip->hd_history != NULL)
+	{
+		anihilation(pip->hd_history);
+		pip->hd_history = NULL;
+	}
+	i = 0;
+	pip->nb_proc = 0;
+	main->buf = getcwd(NULL, 0);
+	if (!main->buf)
+	{
+		if (chdir("/") == -1)
+			return (perror("Error "), 1);
+		while (pip->save_path[i])
 		{
-			anihilation(pip.hd_history);
-			pip.hd_history = NULL;
-		}
-		pip.nb_proc = 0;
-		buf = getcwd(NULL, 0);
-		if (!buf)
-		{
-			if (chdir("/") == -1)
+			if (chdir(pip->save_path[i]) == -1)
 				return (perror("Error "), 1);
-			while (pip.save_path[i])
-			{
-				if (chdir(pip.save_path[i]) == -1)
-					return (perror("Error "), 1);
-				i++;
-			}
-			buf = getcwd(NULL, 0);
-			i = 0;
+			i++;
 		}
-		buff = ft_strjoin(buf, "$ ");
-		free(buf);
-		rd_line = readline(buff);
-		free(buff);
-		if (rd_line == NULL)
-		{
-			free(rd_line);
-			free_all(&pip);
-			return (quit(), 1);
-		}
-		rd_line = verif_read(rd_line, &pip);
-		if (rd_line == NULL)
-		{
-			free_all(&pip);
-			exit(g_status);
-		}
-		free(cmd);
-		if (historic_fct(rd_line, &pip) == 1)
-			return (free_all(&pip), anihilation(pip.hd_history), free(rd_line), 1);
-		free(rd_line);
+		main->buf = getcwd(NULL, 0);
 	}
 	return (0);
 }
